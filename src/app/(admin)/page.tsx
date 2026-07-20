4106c1c818c8e070b221a3b17c137ea4bfc22e02
gaida-lisongo <inbtpkinshasa@gmail.com>
2026-07-20 10:17:41 +0100

    UI meric OK

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -18,13 +18,11 @@ export default function Ecommerce() {
   return (
     <div className="grid grid-cols-12 gap-4 md:gap-6">
       <div className="col-span-12 space-y-6 xl:col-span-7">
-        <SensorMetrics />
-
         <MonthlySalesChart />
       </div>
 
       <div className="col-span-12 xl:col-span-5">
-        <MonthlyTarget />
+        <SensorMetrics />
       </div>
 
       <div className="col-span-12">
--- src/components/metrics/index.tsx
+++ src/components/metrics/index.tsx
@@ -25,7 +25,7 @@ export const SensorMetrics = () => {
   const isHumCritical = humRatio >= 80;
 
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6">
+    <div className="grid grid-cols-1 gap-4 md:gap-6">
       {/* <!-- Metric Item : Température --> */}
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
         <div className="flex items-center justify-between">
